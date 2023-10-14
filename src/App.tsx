14407021df05cc3460e4cbc1fd031814bf2f35ff
Ummenthala Anish <ummenthala.anish@gmail.com>
2023-10-15 01:40:44 +0530

    Added backend API call for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import './App.css';
 
+import axios from 'axios';
 import reactLogo from './assets/react.svg';
 import { useState } from 'react';
 import viteLogo from '/vite.svg';
 
 function App() {
 	const [count, setCount] = useState(0);
+
+	const fetchData = async () => {
+		try {
+			const response = await axios({
+				method: 'get',
+				headers: {
+					'Content-Type': 'application/json',
+					'AUTH-TOKEN':
+						'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjEsImV4cGlyeVRpbWUiOiIyMDIzLTEwLTAzIDIwOjA5OjA5LjExMjkxMyIsImNyZWF0aW9uVGltZSI6IjIwMjMtMTAtMDMgMTk6MDk6MDkuMTEyOTEzIn0.gJhTMX2JikpvFayTE-1QctzC5lm1JyybadetFkQ39OM',
+				},
+				url: '/api/levels/',
+			});
+
+			console.log('data: ', response.data);
+		} catch (error) {
+			console.log('error', error);
+		}
+	};
 
 	return (
 		<>
@@ -22,6 +41,9 @@ function App() {
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
+				<br />
+				<br />
+				<button onClick={() => fetchData()}>API Call</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'./runtimeConfig': './runtimeConfig.browser',
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	return defineConfig({
+		server: {
+			port: parseInt(process.env.VITE_PORT),
+			proxy: {
+				'/api': {
+					target: process.env.VITE_BACKEND_API_URL,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, ''),
+					secure: false,
+				},
+			},
 		},
-	},
-});
+		resolve: {
+			alias: {
+				'./runtimeConfig': './runtimeConfig.browser',
+			},
+		},
+		plugins: [react()],
+	});
+};
