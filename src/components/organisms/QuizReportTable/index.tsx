8261a1833684cee2275b38e06ae65f25652cfb0f
Ummenthala Anish <ummenthala.anish@gmail.com>
2023-10-22 13:46:43 +0530

    Minor bug fixes in report page UI 🐞

--- src/components/organisms/QuizReportTable/index.tsx
+++ src/components/organisms/QuizReportTable/index.tsx
@@ -8,7 +8,7 @@ export interface QuizReportTableProps {
 const QuizReportTable: FC<QuizReportTableProps> = ({ quizReport }) => {
   return (
     <div className="flex flex-col gap-2 px-3 py-2 border rounded-lg bg-darkBlack border-gold tablet:px-10 desktop:px-14">
-      <div className="grid grid-cols-3 gap-2 text-sm font-bold border-b border-[#636363] tablet:text-[18px] tablet:min-w-[800px]">
+      <div className="grid grid-cols-3 gap-2 text-sm font-bold border-b border-[#636363] tablet:text-[18px] desktop:min-w-[800px]">
         <div className="p-2 text-center tablet:p-6" />
         <div className="p-2 text-center tablet:p-6">Classwork</div>
         <div className="p-2 text-center tablet:p-6">Homework</div>
--- src/components/organisms/TestReportTable/index.tsx
+++ src/components/organisms/TestReportTable/index.tsx
@@ -8,9 +8,9 @@ export interface TestReportTableProps {
 const TestReportTable: FC<TestReportTableProps> = ({ testReport }) => {
   return (
     <div className="flex flex-col gap-2 px-3 py-2 border rounded-lg bg-darkBlack border-gold tablet:px-10 desktop:px-14">
-      <div className="grid grid-cols-3 gap-2 text-sm font-bold tablet:text-[18px] tablet:min-w-[800px]">
+      <div className="grid grid-cols-3 gap-2 text-sm font-bold tablet:text-[18px] desktop:min-w-[800px]">
         <div className="p-2 text-center tablet:p-6">Test</div>
-        <div className="p-2 text-center tablet:p-6">
+        <div className="p-2 font-normal text-center tablet:p-6">
           {testReport.Test > 0 ? (
             <span>{testReport.Test}%</span>
           ) : (
