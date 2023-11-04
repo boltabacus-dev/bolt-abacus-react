e2c039145094b007b618098b34ab2301b00e1a6a
Ummenthala Anish <ummenthala.anish@gmail.com>
2023-11-04 23:53:58 +0530

    Admin dashboard updated 🔄️

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,11 +1,31 @@
+import {
+  ADMIN_ADD_BATCH,
+  ADMIN_ADD_QUESTION,
+  ADMIN_ADD_STUDENT,
+  ADMIN_ADD_TEACHER,
+  ADMIN_VIEW_QUIZ,
+} from '@constants/routes';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 export interface AdminDashboardPageProps {}
 
 const AdminDashboardPage: FC<AdminDashboardPageProps> = () => {
   return (
     <div className="flex flex-col gap-4 px-6 py-2 justify-evenly tablet:flex-row tablet:justify-between tablet:items-center tablet:p-10 desktop:px-24">
-      AdminDashboardPage
+      <p className="hidden mt-32 text-lg font-bold text-gold tablet:block">
+        Please use the links on the left to perform desired operations
+      </p>
+      <div className="flex flex-col w-full gap-4 font-bold tablet:hidden">
+        <div className="text-lg text-gold">
+          Please use the below links to perform desired operations
+        </div>
+        <Link to={ADMIN_ADD_STUDENT}>Add Student</Link>
+        <Link to={ADMIN_ADD_TEACHER}>Add Teacher</Link>
+        <Link to={ADMIN_ADD_BATCH}>Add Batch</Link>
+        <Link to={ADMIN_VIEW_QUIZ}>View Quiz</Link>
+        <Link to={ADMIN_ADD_QUESTION}>Add Question</Link>
+      </div>
     </div>
   );
 };
