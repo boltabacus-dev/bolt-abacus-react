b6fec9cf506e4b5a40d4df867834082e2298cbbf
Anish Ummenthala <ummenthala.anish@gmail.com>
2025-03-14 14:20:01 -0700

    color changes

--- src/components/molecules/PracticeCard/index.tsx
+++ src/components/molecules/PracticeCard/index.tsx
@@ -19,7 +19,7 @@ const PracticeCard: FC<PracticeCardProps> = ({
   return (
     <Link
       to={link}
-      className={`tablet:gap-6 flex flex-col justify-center items-center gap-4 bg-card p-4 rounded-lg w-full text-black text-lg ${color === 'blue' && 'bg-blue/70'} ${color === 'green' && 'bg-green/60'} ${color === 'pink' && 'bg-purple/70'} ${color === 'red' && 'bg-red/60'}`}
+      className={`tablet:gap-6 flex flex-col justify-center items-center gap-4 bg-card p-4 rounded-lg w-full text-black text-lg ${color === 'blue' && 'bg-orange/60'} ${color === 'green' && 'bg-green/60'} ${color === 'pink' && 'bg-purple/60'} ${color === 'red' && 'bg-red/50'}`}
     >
       <img
         src={image}
